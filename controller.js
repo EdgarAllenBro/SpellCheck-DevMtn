1a0cf4730fd784f9c437d0764967621f4715d49a
Cameron Grieder <cjgrieder@gmail.com>
2022-01-25 09:55:07 -0700

    added more styling and background images

--- controller.js
+++ controller.js
@@ -4,7 +4,6 @@ module.exports = {
 save: (req,res)=>{
     savedList.push(req.body.name)
     savedList.sort()
-    console.log(savedList)
     res.status(200).send(savedList)},
 
 getsaved: (req ,res)=>{
